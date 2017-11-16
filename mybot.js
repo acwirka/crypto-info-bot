d4abff05a88c1a1630068d5df09e68b56dfb6529
acwirka <cwirkaedu@gmail.com>
2017-11-16 10:13:53 -0800

    cryptocompare

--- mybot.js
+++ mybot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const config = require("./config.json");
-const ticker = "https://api.coinmarketcap.com/v1/ticker/";
+const ticker = "https://min-api.cryptocompare.com/data/";
 const prefix = "!";
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
@@ -14,22 +14,23 @@ client.on("message", (message) => {
 	const command = args.shift().toLowerCase();
 
  	if (command === 'c') {
- 		var coin = args[0];
- 		
+ 		var coin = args[0].toUpperCase();
+ 		console.log(coin);
+ 		var coinPrice = ("price?fsym=" + coin + "&tsyms=BTC");
  		var xhr = new XMLHttpRequest();
-		xhr.open('GET', ticker + coin, true);
+		xhr.open('GET', ticker + coinPrice, true);
 		xhr.send();
  
 		xhr.onreadystatechange = processRequest;
 		function processRequest(e) {
     		if (xhr.readyState == 4 && xhr.status == 200) {
         		var response = JSON.parse(xhr.responseText);
-        		console.log(response[0].name);
-        		message.channel.send(response[0].name);
-        		message.channel.send(response[0].symbol);
-        		message.channel.send(response[0].price_usd);
-        		message.channel.send(response[0].price_btc);
-        		message.channel.send(response[0].percent_change_1h);
+        		console.log(response);
+        		message.channel.send(response.BTC);
+        		// message.channel.send(response[0].symbol);
+        		// message.channel.send(response[0].price_usd);
+        		// message.channel.send(response[0].price_btc);
+        		// message.channel.send(response[0].percent_change_1h);
         		
     		}
 		}
